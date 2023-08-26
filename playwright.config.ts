2e51390f057981cacb588e23f9fe7abd22955d24
Damian Krotowski <damian.krotowski1996@gmail.com>
2023-08-26 19:36:54 +0200

    chore: adding env

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://momentous-rambunctious-bellflower.glitch.me/',
+    baseURL: 'https://groovy-chartreuse-ocelot.glitch.me/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
